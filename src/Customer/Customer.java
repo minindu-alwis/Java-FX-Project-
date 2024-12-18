73e1d9b0227f9fcb743390198c3b9fb9b654b5ee
minindu-alwis <minindualwis@gmail.com>
2024-12-18 14:04:20 +0530

    first commit

---
+++ src/Customer/Customer.java
@@ -0,0 +1,58 @@
+package Customer;
+
+public class Customer {
+
+   private String name;
+   private String age;
+   private String address;
+   private Double salary;
+
+    public Customer(String name, String age, String address, Double salary) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.salary = salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", address='" + address + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
--- src/javaclass/firstfx.java
+++ src/javaclass/firstfx.java
@@ -2,13 +2,33 @@ package javaclass;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import Customer.Customer;
+
+import java.util.AbstractList;
+import java.util.ArrayList;
 
 public class firstfx {
-    public Label hellolbl;
 
-    public void btnClickmeOnAction(ActionEvent actionEvent) {
-        System.out.println("Hello World");
-        hellolbl.setText("Hello Java Fx");
+    public TextField txtname;
+    public TextField txtAge;
+    public TextField txtAddress;
+    public TextField txtSalary;
+
+    ArrayList<Customer> customerList = new ArrayList<>();
+    public void btnCutomerAddOnAction(ActionEvent actionEvent) {
+
+        String name=txtname.getText();
+        String age=txtAge.getText();
+        String address=txtAddress.getText();
+        Double salary=Double.parseDouble(txtSalary.getText());
+
+        Customer c1=new Customer(name,age,address,salary);
+
+
+        customerList.add(c1);
+        System.out.println(customerList);
+
 
     }
 }
